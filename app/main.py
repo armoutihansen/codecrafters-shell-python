da5ee7830a43550e2b68cb9fd8cd1561890d1e8f
Jesper Armouti-Hansen <jesper@armoutihansen.xyz>
2026-03-16 09:46:55 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
+    cmds = ["echo", "exit", "type"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -11,6 +12,11 @@ def main():
             break
         elif command.split()[0] == "echo":
             print(" ".join(command.split()[1:]))
+        elif command.split()[0] == "type":
+            if command.split()[1] in cmds:
+                print(f"{command.split()[1]} is a shell builtin")
+            else:
+                print(f"{command.split()[1]}: not found")
         else:
             print(f"{command}: command not found")
     
