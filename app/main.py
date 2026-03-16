ebb1ee4ee262149509038f23f7a9aa77fbf174d0
Jesper Armouti-Hansen <jesper@armoutihansen.xyz>
2026-03-16 11:11:54 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def main():
                 print(f"{arg}: not found")
         elif is_valid_command(command.split()[0], paths):
             result = subprocess.run(command.split())
-            print(result.stdout.decode())
+            # print(result.stdout.decode())
             # print(f"Program was passed with {len(command.split())} args (including the program name).")
             # print(f"Arg #0 (program name): {command.split()[0]}")
             # for i, arg in enumerate(command.split()[1:], start=1):
